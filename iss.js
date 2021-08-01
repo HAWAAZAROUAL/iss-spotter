651b9b1da966a493518e6af5f00da7bd769237d4
Hawaa <hawaazaroual@gmail.com>
2021-07-31 21:43:28 -0600

    ISS completed, next fly times works

--- iss.js
+++ iss.js
@@ -12,25 +12,25 @@ const fetchMyIP = function(callback) {
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
-  const fetchCoordsByIP = function(ip, callback) {
-    request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
-      if (error) {
-        callback(error, null);
-        return;
-      }
-  
-      if (response.statusCode !== 200) {
-        callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
-        return;
-      }
-  
-      const { latitude, longitude } = JSON.parse(body);
-  
-      callback(null, { latitude, longitude });
-    });
-  };
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+
+    // error: request failed
+    if (error) return callback(error, null);
+
+    // non-200: assume server error
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates: ${body}`), null);
+      return;
+    }
+    
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
 const fetchISSFlyOverTimes = function(coords, callback) {
   const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
 
